b4507e30a5126d38c5042cfe001197d9cb6fe3a6
算法杰哥 <qkj1205@163.com>
2023-01-17 18:46:01 +0800

    提取文件名成功

--- boost_searcher/parser.cc
+++ boost_searcher/parser.cc
@@ -8,6 +8,8 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <cassert>
+#include <boost/filesystem.hpp>
 
 // 下面是路径
 const std::string src_path = "data/input";
@@ -20,16 +22,56 @@ typedef struct DocInfo
   std::string url;     // 该文档在官网的url
 } DocInfo_t;
 
-bool EnumFile(const std::string& src_path, std::vector<std::string>* p_files_list)
+// 保存src_path路径下所有文件名 到 files_list 中
+bool EnumFile(const std::string& src_path, std::vector<std::string>* files_list)
 {
-  (void)src_path;
-  (void)p_files_list;
-  return false;
+  namespace fs = boost::filesystem;
+
+  // 构建一个对象
+  fs::path root_path(src_path);
+  // 判断 路径是不是存在
+  if(!fs::exists(root_path))
+  {
+    std::cerr<< "文件路径不存在" << std::endl;
+    return false;
+  }
+
+  // 定义一个空的迭代器
+  fs::recursive_directory_iterator end;
+  for(fs::recursive_directory_iterator iter(root_path); iter != end; iter++)
+  {
+    // 此时我们遍历所有的文件
+    // 筛选出常规文件
+    if(!fs::is_regular_file(*iter)) 
+    {
+      continue; 
+    }
+    // 检测带路径的文件名
+    if(!(iter->path().extension() == ".html"))
+    {
+      continue; 
+    }
+    // std::cout << iter->path().string() <<std::endl;
+    // 当前的路径一定是一个以.html的普通文件
+    files_list->push_back(iter->path().string());
+  }
+
+  return true;
 }
 
+// 2. 去标签,把 files_list 中所有的文件内容保存到一个结构体中,
+// 把结构体放在 result中
 bool ParseHtml(const std::vector<std::string> files_list, std::vector<DocInfo_t> *result)
 {
-
+  assert(result);
+  for(const std::string& file : files_list)
+  {
+    // 1. 读取文件
+    
+    // 2. 提取文件 tile 
+    // 3. 提取content
+    // 4. 提取 特定的文件路径,构建  url 
+  }
 }
 
 bool SaveHtml(const std::vector<DocInfo_t>& result, const std::string& output)
