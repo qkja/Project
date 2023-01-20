3913d425b17d92ebccae6121c77ba47364bf5c30
算法杰哥 <qkj1205@163.com>
2023-01-20 19:11:35 +0800

    倒排索引 正牌索引框架建立

--- boost_searcher/index.hpp
+++ boost_searcher/index.hpp
@@ -1,8 +1,9 @@
-#pragma once 
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <fstream>
 
 namespace ns_index
 {
@@ -17,54 +18,85 @@ namespace ns_index
   struct InvertedElem
   {
     uint64_t doc_id;
-    std::string word;// 关键字
-    int weight; // 权重
+    std::string word; // 关键字
+    int weight;       // 权重
   };
 
   class Index
   {
-    public:
-      // 倒排拉链
-      typedef std::vector<InvertedElem> InvertedList;
-    public:
-      Index(){};
-      ~Index(){};
-    public:
-      // 根据 id 找到文档内容
-      DocInfo* GetForwardIndex(uint64_t id)
+  public:
+    // 倒排拉链
+    typedef std::vector<InvertedElem> InvertedList;
+
+  public:
+    Index(){};
+    ~Index(){};
+
+  public:
+    // 根据 id 找到文档内容
+    DocInfo *GetForwardIndex(uint64_t id)
+    {
+      if (id < 0 || id >= forward_index.size())
       {
-        if(id <0 || id >= forward_index.size())
+        std::cerr << "id越界了" << std::endl;
+        return nullptr;
+      }
+
+      return &(forward_index[id]);
+    }
+
+    // 根据关键子找到倒排拉链
+    InvertedList *GetInvertedList(const std::string &key)
+    {
+      auto it = inverted_index.find(key);
+      if (it == inverted_index.end())
+      {
+        std::cerr << key << "没有倒排拉链" << std::endl;
+        return nullptr;
+      }
+      return &(it->second);
+    }
+
+    // 根据去标签之后的文档正派和倒排索引
+    bool BuildIndex(const std::string &input)
+    {
+
+      std::ifstream in(input, std::ios::in | std::ios::binary);
+      if (!in.is_open())
+      {
+        std::cerr << "你给的路径是不正确的" << std::endl;
+        return false;
+      }
+      std::string line;
+      while (std::getline(in, line))
+      {
+        // 这里添加到正牌索引中
+        DocInfo *doc = BuildForwardIndex(line);
+        if (doc == nullptr)
         {
-          std::cerr << "id越界了"<<std::endl;
-          return nullptr;
+          std::cerr << "建立正牌索引失败 " << line << std::endl;
+          continue;
         }
 
-        return &(forward_index[id]);
+        // 建立倒排索引
+        BuildInvertedIndex(*doc);
       }
+      return true;
+    }
 
-      // 根据关键子找到倒排拉链
-      InvertedList* GetInvertedList(const std::string& key)
-      {
-        auto it = inverted_index.find(key);
-        if(it == inverted_index.end())
-        {
-          std::cerr << key<<"没有倒排拉链"<<std::endl;
-          return nullptr;
-        }
-        return &(it->second);
-      }
+  private:
+    DocInfo *BuildForwardIndex(const std::string &line)
+    {
+      return nullptr;
+    }
+    void BuildInvertedIndex(const DocInfo &doc)
+    {
+    }
 
-      // 根据去标签之后的文档正派和倒排索引
-      bool BuildIndex(const std::string& input)
-      {
-
-        return true;
-      }
-
-    private:
-      // 正派索引 -- 通过id找内容  用数字的下标来做文档的id
-      std::vector<DocInfo> forward_index;
-      // 倒排索引 -- 关键字 找道文档id ,id可能有多个 
-      std::unordered_map<std::string, InvertedList> inverted_index;
+  private:
+    // 正派索引 -- 通过id找内容  用数字的下标来做文档的id
+    std::vector<DocInfo> forward_index;
+    // 倒排索引 -- 关键字 找道文档id ,id可能有多个
+    std::unordered_map<std::string, InvertedList> inverted_index;
   };
 }
