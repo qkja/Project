d70ea2d48c7efe442596fdf9caaf62505ea78961
算法杰哥 <qkj1205@163.com>
2023-01-28 18:43:40 +0800

    html骨架完成

--- boost_searcher/http_server.cc
+++ boost_searcher/http_server.cc
@@ -4,11 +4,19 @@
 * Date: 2023-01-28
 * Time: 15:27
 */
+
 #include "cpp-httplib/httplib.h"
+#include "searcher.hpp"
+
+const std::string root_path = "./wwwroot";
 
 int main()
 {
-  httplib::Server svr;
+  ns_srearchre::Searcher searcher;
 
+  httplib::Server svr;
+  svr.set_base_dir(root_path.c_str()); // 设置web根目录
+
+  svr.listen("0.0.0.0", 8081);
   return 0;
 }
