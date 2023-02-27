f1d0064d45771025a88351c4b7573005d988a504
算法杰哥 <qkj1205@163.com>
2023-02-27 20:39:45 +0800

    数据清洗 框架搭建成功

--- boost_searcher/parser.cc
+++ boost_searcher/parser.cc
@@ -1,225 +1,53 @@
 /**
-* User: Qkj
-* Description: 进行去标签工作
-* Date: 2023-02-05
-* Time: 11:00
-*/
+ * User: Qkj
+ * Description:  去标签/数据清洗
+ * Date: 2023-02-27
+ * Time: 20:08
+ */
 
 #include <iostream>
 #include <string>
 #include <vector>
-#include <boost/filesystem.hpp>
 
-#include "util.hpp"
+// 这是一个目录,下面放的是所有的html网页
+const std::string src_path = "data/input";
 
+// 下面是一个文本文件,该文件保存所有的 网页清洗后的数据
+const std::string output = "data/raw_html/raw.txt";
 
-// 这是源目录 放的是所有的 html文件
-const std::string src_path = "./data/input"; 
-
-// 保存所有去标签的html 文件
-const std::string output = "./data/raw_html/raw.txt";
-
+// 解析网页格式
 typedef struct DocInfo
 {
-  std::string title;
-  std::string content;
-  std::string url;
+  std::string title;   // 文档标题
+  std::string content; // 文旦内容
+  std::string url;     // 该文档在官网的的url
 } DocInfo_t;
 
-bool EnumFile(const std::string& src_path, std::vector<std::string>* file_list)
+int main(void)
 {
-  //std::cout << "=====================" << std::endl;
-  //std::cout <<src_path << std::endl;
-  //std::cout << "=====================" << std::endl;
-  namespace fs = boost::filesystem;
-  
-  fs::path root_path(src_path);  // 构建一个 path 对象  这里是局部优先,没有问题
-
-  if(!fs::exists(root_path))
-  {
-    std::cerr << src_path << " 不存在" << std::endl;
-    return false;
-  }
-
-  // 定义一个 空的迭代器 
-  fs::recursive_directory_iterator end;
-  for(fs::recursive_directory_iterator iter(root_path); iter != end; iter++)
-  {
-    // 注意这里是所有的文件 我们必须只要*html文件
-    // 刷选常规文件
-    if(!fs::is_regular_file(*iter))
-    {
-      continue;
-    }
-
-    // 拿到 *html 文件
-    if((iter->path().extension() != ".html"))
-    {
-      continue;
-    }
-
-    //if((*file_list).size()%100 == 0)
-    //{
-    //  std::cout << "debug " << iter->path().string() <<std::endl;
-    //}
-    // 此时一定是 .html 文件
-    file_list->push_back(iter->path().string());
-  }
-  //std::cout << file_list->size() << std::endl;
-  return true;
-}
-
-static bool ParseTitle(const std::string& result, std::string* title)
-{
-  //搜索 title 关键字
-  std::size_t begin = result.find("<title>");
-  if(begin == std::string::npos)
-  {
-    return false;
-  }
-  std::size_t end = result.find("</title>");
-  if(end == std::string::npos)
-  {
-    return false;
-  }
-  begin += std::string("<title>").size();
-
-  if(begin>=end)
-    return false;
-
-  *title = result.substr(begin, end-begin);
-  return true;
-}
-
-
-static bool ParseContent(const std::string& result, std::string* content)
-{
-  enum status
-  {
-    LABLE,
-    CONTENT
-  };
-
-  //std::cout <<"你好  " << result  << std::endl;
-  enum status s = LABLE;
-
-  for(char ch:result)
-  {
-    //std::cout <<"你好  "<< ch  << std::endl;
-    switch(s)
-    {
-      case LABLE:
-        if(ch == '>') 
-          s = CONTENT;
-        break;
-      case CONTENT:
-        if(ch == '<')
-        {
-          s = LABLE;
-        }
-        else 
-        {
-          // 问题  ch 会 拿到 \n吗 
-          // 准确说 getline会不会拿到\n 不会  所以 这里我们可以说是不用担心
-          if(ch == '\n')
-            ch = ' ';
-          content->push_back(ch);
-        }
-        break;
-    }
-  }
-
-  return true;
-}
-
-static bool ParseUrl(const std::string& file_path, std::string* url)
-{
-  std::string url_head = "https://www.boost.org/doc/libs/1_78_0/doc/html";
-  std::string url_tail = file_path.substr(src_path.size()); 
-  *url = url_head + url_tail;
-  return true;
-}
-
-static void ShowDoc(const DocInfo_t& doc)
-{
-  std::cout << "标题: " <<doc.title << std::endl;
-  std::cout << "内容: " <<doc.content << std::endl;
-  std::cout << "url : " <<doc.url << std::endl;
-}
-
-bool ParseHtml(const std::vector<std::string>& file_list, std::vector<DocInfo_t>* results)
-{
-  std::string result;
-  for(const std::string& file:file_list)
-  {
-    // 1. 读取文件
-    if(!ns_util::FileUtil::ReadFile(file, &result))
-    {
-      continue;
-    }
-
-    DocInfo_t doc;
-    // 2. 提取title
-    if(!ParseTitle(result, &doc.title))
-    {
-      continue;
-    }
-    // 3. 提取content 本质就是去标签
-    if(!ParseContent(result, &doc.content))
-    {
-      continue;
-    }
-    // 4. 构建/提取 url
-    if(!ParseUrl(file, &doc.url))
-    {
-      continue;
-    }
-    
-    // for debug
-    if(results->size() == 1 || results->size() == 10 
-        || results->size() == 15 || results->size() == 50)
-    {
-
-      //std::cout << result << std::endl;
-      ShowDoc(doc);
-    }
-
-    results->push_back(std::move(doc));
-    result.clear();
-  }
-  return true;
-}
-
-bool SaveHtml(const std::vector<DocInfo_t>& results, const std::string& output)
-{
-
-  return true;
-}
-
-int main()
-{
-  // 1. 保存所有的 文件名
+  // 保存所有的 html 的文件名
   std::vector<std::string> file_list;
-  if(!EnumFile(src_path, &file_list))
+
+  // 第一步: EnumFile 枚举所有的文件名(带路径),仅限 网页,方便后期对一个一个文件进行读取
+  if (false == EnumFile(src_path, &file_list))
   {
-    std::cerr << "保存文件名失败 " << std::endl;
+    std::cerr << "枚举文件名失败" << std::endl;
     return 1;
   }
 
-  // 2. 读取每一个文件的内容
+  // 第二部:读取每一个文件的内容,进行解析,解析的格式 为DocInfo_t
   std::vector<DocInfo_t> results;
-  if(!ParseHtml(file_list, &results))
+
+  if (false == ParseHtml(file_list, &results))
   {
-    std::cerr << "拿到文件内容失败" <<std::endl;
+    std::cerr << "解析文件失败" << std::endl;
     return 2;
   }
-
-  // 3. 把解析完毕的内容写入到output中, 按照\3作为分割符
-  if(!SaveHtml(results, output))
+  // 第三步: 把解析文件的内容写入到output中,按照\3\n 作为每一个文档的分割符
+  if(false == SaveHtml(results, &output))
   {
-    std::cerr << "保存文件失败" <<std::endl;
+    std::cerr << "保存文件失败" << std::endl;
     return 3;
   }
-
   return 0;
 }
