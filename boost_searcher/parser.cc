a826dd73b04b3f351db5761e1a8134a1744053fb
算法杰哥 <qkj1205@163.com>
2023-02-27 21:14:03 +0800

    数据清洗 框架搭建完成

--- boost_searcher/parser.cc
+++ boost_searcher/parser.cc
@@ -23,6 +23,30 @@ typedef struct DocInfo
   std::string url;     // 该文档在官网的的url
 } DocInfo_t;
 
+/// @brief  把目录下的所有html 文件名 保存到 数组中
+/// @param src_path 目录
+/// @param file_list 数组指针,数组中保存文件名
+/// @return 成功返回ture,否则就是false
+static bool EnumFile(const std::string &src_path, std::vector<std::string> *file_list)
+{
+}
+
+/// @brief 读取数组中的每一个文件里面的内容,把他保存到一个结构体中,其中结构体放在数组中
+/// @param file_list 保存文件名的数组
+/// @param results  保存结构体的数组指针
+/// @return  成功返回ture,否则就是false
+static bool ParseHtml(const std::vector<std::string> &file_list, std::vector<DocInfo_t> *results)
+{
+}
+
+/// @brief 把数组结构体的内容保存按照一定的格式保存到 文件中
+/// @param results 结构体数组
+/// @param output  文件名
+/// @return 成功返回ture,否则就是false
+static bool SaveHtml(const std::vector<DocInfo_t> &results, const std::string *output)
+{
+}
+
 int main(void)
 {
   // 保存所有的 html 的文件名
@@ -44,7 +68,7 @@ int main(void)
     return 2;
   }
   // 第三步: 把解析文件的内容写入到output中,按照\3\n 作为每一个文档的分割符
-  if(false == SaveHtml(results, &output))
+  if (false == SaveHtml(results, &output))
   {
     std::cerr << "保存文件失败" << std::endl;
     return 3;
