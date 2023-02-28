b01e4dc4f99d7b6c3b8373c1f7b0f18432dad6a0
算法杰哥 <qkj1205@163.com>
2023-02-28 23:22:57 +0800

    解析文件函数 框架完成

--- boost_searcher/util.hpp
+++ boost_searcher/util.hpp
@@ -1,7 +1,28 @@
 #ifndef __UTIL_HPP__
 #define __UTIL_HPP__
+#include <string>
+#include <cassert>
+#include <fstream>
 
 // 这是一个工具集
+namespace ns_util
+{
+  /// @brief  这是为了解析文件
+  class FileUtil
+  {
+  public:
+    /// @brief 读物文件内容到 out中
+    /// @param file_path 
+    /// @param out 
+    /// @return 
+    static bool ReadFile(const std::string &file_path, std::string *out)
+    {
+      assert(out);
+      return true;
+    }
 
+
+  };
+
+}
 #endif
-
