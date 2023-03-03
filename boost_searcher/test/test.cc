1d0864b576ea2dd30cdb9248124d6ed4ed887882
算法杰哥 <qkj1205@163.com>
2023-03-03 22:59:16 +0800

    json 串的使用

--- boost_searcher/test/test.cc
+++ boost_searcher/test/test.cc
@@ -7,24 +7,49 @@
 #include <string>
 #include <iostream>
 #include <vector>
-#include <boost/algorithm/string.hpp>
+
+#include <jsoncpp/json/json.h>
 
 int main()
 {
-  std::vector<std::string> results;
-  std::string target = "aaaa\3\3\3\3bbbb\3cccc";
+  Json::Value root;
+  Json::Value item1;
+  item1["key1"] = "value1";
+  item1["key2"] = "value2";
 
-  // 默认的是不压缩
-  //boost::split(results, target, boost::is_any_of("\3"));
-  
-  // 我们不想保留
-  boost::split(results, target, boost::is_any_of("\3"),
-      boost::token_compress_on);
+  Json::Value item2;
+  item2["key1"] = "value1";
+  item2["key2"] = "value2";
 
-  for(auto& e:results)
-  {
-    std::cout << e << std::endl;
-  }
+  root.append(item1);
+  root.append(item2);
 
+  Json::StyledWriter writer;
+  std::string s = writer.write(root);
+  std::cout << s << std::endl;
   return 0;
 }
+
+
+
+//#include <boost/algorithm/string.hpp>
+//
+//int main()
+//{
+//  std::vector<std::string> results;
+//  std::string target = "aaaa\3\3\3\3bbbb\3cccc";
+//
+//  // 默认的是不压缩
+//  //boost::split(results, target, boost::is_any_of("\3"));
+//  
+//  // 我们不想保留
+//  boost::split(results, target, boost::is_any_of("\3"),
+//      boost::token_compress_on);
+//
+//  for(auto& e:results)
+//  {
+//    std::cout << e << std::endl;
+//  }
+//
+//  return 0;
+//}
