d3431903fe25c80b57e206e25fb3d2d2c03e9635
算法杰哥 <qkj1205@163.com>
2023-03-04 11:00:18 +0800

    引入httplib库

--- boost_searcher/http_server.cc
+++ boost_searcher/http_server.cc
@@ -1,22 +1,11 @@
 /**
 * User: Qkj
 * Description: 
-* Date: 2023-01-28
-* Time: 15:27
+* Date: 2023-03-04
+* Time: 10:30
 */
-
-#include "cpp-httplib/httplib.h"
-#include "searcher.hpp"
-
-const std::string root_path = "./wwwroot";
-
 int main()
 {
-  ns_srearchre::Searcher searcher;
 
-  httplib::Server svr;
-  svr.set_base_dir(root_path.c_str()); // 设置web根目录
-
-  svr.listen("0.0.0.0", 8081);
   return 0;
 }
