bad0a408812e939499e682803d5ad44b5fe240dc
算法杰哥 <qkj1205@163.com>
2023-03-04 16:11:07 +0800

    后端基本完成

--- boost_searcher/http_server.cc
+++ boost_searcher/http_server.cc
@@ -1,21 +1,38 @@
 /**
-* User: Qkj
-* Description: 
-* Date: 2023-03-04
-* Time: 10:30
-*/
+ * User: Qkj
+ * Description:
+ * Date: 2023-03-04
+ * Time: 10:30
+ */
 #include "cpp-httplib/httplib.h"
+#include "searcher.hpp"
 
 const std::string root_path = "./wwwroot";
-
+const std::string input = "data/raw_html/raw.txt";
 int main()
 {
+  // 初识话 sercher
+  ns_searcher::Searcher search;
+  search.InitSearcher(input);
 
   httplib::Server svr;
   svr.set_base_dir(root_path.c_str()); // 设置跟目录
-  svr.Get("/hi",[](const httplib::Request& req, httplib::Response& rsp) {
-      rsp.set_content("hello word!", "text/plain; charset=utf-8");
-      });
-  svr.listen("0.0.0.0",8081);
+
+  svr.Get("/s", [&search](const httplib::Request &req, httplib::Response &rsp)
+          {
+            if (req.has_param("word") == false)
+            {
+              rsp.set_content("必须要搜索关键字", "text/plain; charset=utf-8");
+              return;
+            }
+
+            std::string word = req.get_param_value("word");
+            std::string json_string;
+            search.Search(word, &json_string);
+            rsp.set_content(json_string, "application/json");
+            // rsp.set_content("hello word!", "text/plain; charset=utf-8");
+          });
+  svr.listen("0.0.0.0", 8081);
+
   return 0;
 }
