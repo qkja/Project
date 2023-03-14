fdcb4b1f379d8715d48f389e3bfe611d6cce6de9
算法杰哥 <qkj1205@163.com>
2023-03-14 23:31:02 +0800

    数据库句柄的得到和释放结束

--- video_on_demand/source/data.hpp
+++ video_on_demand/source/data.hpp
@@ -4,12 +4,46 @@
 #include "util.hpp"
 #include <mysql/mysql.h>
 #include <mutex>
+#include <assert.h>
 
 namespace aod
 {
-  static MYSQL *MySQLInit();
-  static void MySQLDestroy();
-  static bool MySQLQuery(MYSQL *mysql, const std::string &sql);
+  static MYSQL *MySQLInit()
+  {
+    // 初始化句柄
+    MYSQL *mysql = mysql_init(NULL);
+    assert(mysql);
+
+    // 链接服务器
+    if (NULL == mysql_real_connect(mysql /*句柄*/, "127.0.0.1", "root", "" /*密码*/, "aod_system" /*数据库名称*/, 0 /*0 默认 就是3306*/, NULL, 0))
+    {
+      printf("数据库链接失败\n");
+      mysql_close(mysql);
+      return nullptr;
+    }
+    mysql_set_character_set(mysql, "utf8");
+    return mysql;
+  }
+
+  static void MySQLDestroy(MYSQL *mysql)
+  {
+    if (mysql == nullptr)
+      return;
+    mysql_close(mysql);
+  }
+
+  static bool MySQLQuery(MYSQL *mysql, const std::string &sql)
+  {
+    int ret = mysql_query(mysql, sql.c_str());
+    if (ret != 0)
+    {
+      std::cout << "sql:  " << sql << std::endl;
+      std::cout << mysql_errno(mysql) << std::endl;  
+      return false;
+    }
+    return true;
+  }
+  
   class TableVideo
   {
   public:
